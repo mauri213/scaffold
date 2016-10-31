790f84b41d5ca6aac96c472c9c4be2e05ddae144
Mauricio Orozco <Mauricio Orozco>
2016-10-31 11:58:58 -0400

    Webpack first try

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+function app () {
+		// You can do it
+}
+
+app () ;
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+// webpack.config.js
+module.exports = {
+	entry: './src/js/main.js', //string
+	output: { // object
+		path: './dist', //bundle of files
+		filename: 'bundle.js' //green means good
+	}
+};
