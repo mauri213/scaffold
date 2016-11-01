c4cd007bd8cc8a9ae5c7e7419b5561a49e864f48
Mauricio Orozco <Mauricio Orozco>
2016-11-01 09:31:32 -0400

    Added react

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,18 @@ module.exports = {
 	output: { // object
 		path: './dist', //bundle of files
 		filename: 'bundle.js' //green means good
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/, // only going to load files with .js
+				exclude: /(node_modules)/,
+				query: {
+					presets: ['react']
+
+				}
+			}
+		]
 	}
 };
