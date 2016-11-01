16e98274ff4dd3f20c1bed316a9ad66eddba1eb3
Mauricio Orozco <Mauricio Orozco>
2016-11-01 09:15:41 -0400

    Included notes

---
+++ AppView.js
@@ -0,0 +1,7 @@
+var View = require('./View.js');
+
+function AppView () {
+	View.apply(this, arguments);
+}
+
+module.exports = AppView;
---
+++ View.js
@@ -0,0 +1,7 @@
+var imHidden = true;
+
+function View () {
+	imHidden = !imHidden;
+}
+
+module.exports = View; // node style export. node module pattern
